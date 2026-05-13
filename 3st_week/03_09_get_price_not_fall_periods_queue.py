48a10a6815d27aca919cdc57276b460bf47e8c48
Song1524 <zsdx1525@gmail.com>
2026-05-13 20:03:13 +0900

    0513_codingTest_python

---
+++ 3st_week/03_09_get_price_not_fall_periods_queue.py
@@ -0,0 +1,30 @@
+from collections import *
+
+prices = [1, 2, 3, 2, 3]
+
+
+def get_price_not_fall_periods(prices):
+    # 이 부분을 채워주세요!
+    answer = []
+    que = deque(prices)
+
+    while que:
+        cur = que.popleft()
+        count = 0
+
+        for next_value in que:
+            count += 1
+
+            if cur > next_value:
+                break
+
+        answer.append(count)
+
+    return answer
+
+
+print(get_price_not_fall_periods(prices))
+
+print("정답 = [4, 3, 1, 1, 0] / 현재 풀이 값 = ", get_price_not_fall_periods(prices))
+print("정답 = [6, 2, 1, 3, 2, 1, 0] / 현재 풀이 값 = ", get_price_not_fall_periods([3, 9, 9, 3, 5, 7, 2]))
+print("정답 = [6, 1, 4, 3, 1, 1, 0] / 현재 풀이 값 = ", get_price_not_fall_periods([1, 5, 3, 6, 7, 6, 5]))
